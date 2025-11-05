cc2de1dc7fa02f17bffd623def157eae16ede492
Kai Harries <kai.harries@posteo.de>
2025-11-05 17:03:27 +0100

    Forward start script options to Emacs

--- kais-slim-emacs.sh
+++ kais-slim-emacs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 [[ -e ~/.emacs ]] && echo "[W] Found a ~/.emacs file, please be aware that it might interfere with this configuration."
 dir=$(dirname $(readlink -f $0))
-emacs --init-directory=${dir} --load=${dir}/init.el
+emacs --init-directory=${dir} --load=${dir}/init.el "$@"
